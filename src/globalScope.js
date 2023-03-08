af651ce999d2295872fc76bfce30b0323be90f41
nox <dark.tempus@gmail.com>
2023-03-07 19:59:55 -0500

    firtCommit

---
+++ src/globalScope.js
@@ -0,0 +1,28 @@
+//global scope
+
+var fruit = 'apple';
+
+function bestFruit() {
+
+    console.log(fruit);
+
+}
+
+bestFruit();
+
+
+
+
+function greeting() {
+
+
+    let userName = 'Ana';
+    console.log(userName);
+
+    if (userName === 'Ana') {
+
+        console.log(`hello ${userName}!`)
+    }
+
+}
+greeting();
